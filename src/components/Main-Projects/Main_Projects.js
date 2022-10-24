54a42525c86eb6965a89fc5559944d40c7c12950
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-10-25 07:57:12 +1300

    Finish projects page

--- src/components/Main-Projects/Main_Projects.js
+++ src/components/Main-Projects/Main_Projects.js
@@ -5,6 +5,10 @@ import ProjectShow from "./Projects-Show/ProjectsShow";
 
 // Images
 import DhruvBanking from "../../images/projects/dhruv_banking.png";
+import DhruvData from "../../images/projects/dhruv_data.png";
+import EnviroClubWebsite from "../../images/projects/eviro_club_website.png";
+import HavenBot from "../../images/projects/haven_bot.png";
+import OJCBadesWebsite from "../../images/projects/ojc_badges_website.png";
 
 function MainProjects() {
   return (
@@ -13,6 +17,30 @@ function MainProjects() {
         src={DhruvBanking}
         name="Dhruv Banking"
         desc="The future of banking"
+        GithubUrl="https://github.com/Dhruv-Banking"
+      />
+      <ProjectShow
+        src={DhruvData}
+        name="Dhruv Data"
+        desc="The newest database"
+        GithubUrl="https://github.com/Dhruv-Banking"
+      />
+      <ProjectShow
+        src={EnviroClubWebsite}
+        name="Enviro Club Website"
+        desc="Enviro Club Website"
+        GithubUrl="https://github.com/Dhruv-Banking"
+      />
+      <ProjectShow
+        src={HavenBot}
+        name="Haven Bot"
+        desc="My spicy new discord bot"
+        GithubUrl="https://github.com/Dhruv-Banking"
+      />
+      <ProjectShow
+        src={OJCBadesWebsite}
+        name="OJC Badges Website"
+        desc="A website I made for badges at my previoud school"
         GithubUrl="https://github.com/Dhruv-Banking"
       />
     </main>
