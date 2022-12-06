113fe3729d2c6870552080541de72653edd70ad1
dhruv <dhruvrayat50@gmail.com>
2022-12-06 16:00:43 +1300

    Update: Updated the links to make them work.

--- src/components/Splash/SplashChild.jsx
+++ src/components/Splash/SplashChild.jsx
@@ -1,10 +1,21 @@
 import "./SplashChild.css";
+import { Link } from "react-router-dom";
 
 function SplashChild(props) {
   return (
     <header className="splash_child">
       <h1>{props.PageName}</h1>
-      <p>{props.LinkTo}</p>
+      <p>
+        <Link
+          to="/"
+          onClick={() => {
+            window.scrollBy(0, -100000);
+          }}
+          className="link_home"
+        >
+          {props.LinkTo}
+        </Link>
+      </p>
     </header>
   );
 }
