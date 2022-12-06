7cdc7a3a41b91ea025adda1eeaa109b7162e758e
dhruv <dhruvrayat50@gmail.com>
2022-12-06 15:53:26 +1300

    Update: Added bold text to about me page

--- src/components/Main-About/Main_About.jsx
+++ src/components/Main-About/Main_About.jsx
@@ -17,19 +17,34 @@ function MainAbout() {
         <h3>2022</h3>
         <ul>
           <li>
-            Programming contest - Programming challenge hosted at Auckland
-            University
+            <strong>Programming contest</strong> - Programming challenge hosted
+            at Auckland University
           </li>
-          <li>Epro8 - Enginnering challenge</li>
-          <li>Dhruv Banking - Comp Sci assesment</li>
-          <li>OSC Enviro Club Website - Creating a website for my club</li>
+          <li>
+            <strong>Epro8</strong> - Enginnering challenge
+          </li>
+          <li>
+            <strong>Dhruv Banking</strong> - Comp Sci assesment
+          </li>
+          <li>
+            <strong>OSC Enviro Club Website</strong> - Creating a website for my
+            club
+          </li>
         </ul>
 
         <h3>2021</h3>
         <ul>
-          <li>Manutaki - Student Leader</li>
-          <li>OJC Flight Simulator - Hosting a Flight Simulator club</li>
-          <li>OJC In VR - Presenting our model to primaty students</li>
+          <li>
+            <strong>Manutaki</strong> - Student Leader
+          </li>
+          <li>
+            <strong>OJC Flight Simulator</strong> - Hosting a Flight Simulator
+            club
+          </li>
+          <li>
+            <strong>OJC In VR</strong> - Presenting our model to primaty
+            students
+          </li>
         </ul>
       </div>
     </main>
